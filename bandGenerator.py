831152bb7215ff72121ef3ca4815df879401e0be
Daniel Fraga <dan.fraga33@gmail.com>
2023-11-29 11:07:05 +0000

    feat:  band generator app

---
+++ bandGenerator.py
@@ -0,0 +1,6 @@
+# Band Name Generator
+
+print("Hello! Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in ? \n")
+pet = input("What is the name of a pet? \n")
+print('Your brand name could be' + ' ' + city + ' ' + pet + '\n')
